bb173ef3b77f2a908a640f09a4ca4ac5880a13d4
ariel murphy <arielladders@gmail.com>
2020-08-03 22:51:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,11 +9,11 @@ class Artist
     Song.all.select {|song| song.artist == self }
   end 
   
-  def add_song(song)
+  def add_song(song) #add
     song.artist = self 
   end 
   
-  def add_song_by_name(name)
+  def add_song_by_name(name) #add by name
     song = Song.new(name)
     add_song(song)
   end 
